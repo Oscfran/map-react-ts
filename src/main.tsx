c125bf1c8b4176b511616428f25482df58151873
Oscfran <62187252+Oscfran@users.noreply.github.com>
2025-02-25 18:45:59 -0600

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
+import MapApi from "./Components/MapView/Map.tsx";
+import Layout from "./Components/Pages/Layout.tsx";
+import NoPage from "./Components/Pages/NoPage.tsx";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<MapApi />} />
+					<Route path="*" element={<NoPage />} />{" "}
+					{/* Catch-all for unknown routes */}
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 );
